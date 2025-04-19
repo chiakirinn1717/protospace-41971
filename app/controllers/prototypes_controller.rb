02e7177a74024cd5bf4b67ec08d6f50e1c4ece8e
chiakirinn1717 <chiakirinn1717@gmail.com>
2025-04-19 18:27:27 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,57 @@
+class PrototypesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
+
+  def index
+    @prototypes = Prototype.includes(:user).all
+  end
+
+  def show
+    @prototype = Prototype.find(params[:id])
+    @comment = Comment.new
+    @comments = Comment.all
+  end
+
+  def create
+    @prototype = Prototype.create(prototype_params)
+    if @prototype.save
+      redirect_to "/"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  #以下ログインが必要
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+    unless user_signed_in?
+      redirect_to "/"
+    end
+  end
+
+  def destroy
+    prototype = Prototype.find(params[:id])
+    prototype.destroy
+    redirect_to "/"
+  end
+
+  private 
+  
+  def prototype_params
+    params.require(:prototype).permit(:title,:catch_copy,:concept,:image).merge(user_id: current_user.id)
+  end
+
+end
